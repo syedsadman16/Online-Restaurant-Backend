fcd065f006b9977f109fd491b91e7c8a771e20c7
smfils1 <smfils1@gmail.com>
2020-10-21 12:32:31 -0400

    Fixed user endpoint auth and added userService

--- src/main/java/com/cs322/ors/controller/UserController.java
+++ src/main/java/com/cs322/ors/controller/UserController.java
@@ -1,54 +1,58 @@
 package com.cs322.ors.controller;
 
-import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cs322.ors.security.UserPrincipal;
+import com.cs322.ors.model.User;
+import com.cs322.ors.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin
 public class UserController {
 
+	@Autowired
+	UserService userService;
+	
 	@GetMapping
 	@PreAuthorize("hasRole('MANAGER')")
-	public String accessUsers(Principal user) {
-		
-		return ("<h1> user info <h1>");
+	public List<User> accessUsers() {
+		return userService.getAllUsers();
 	}
 		
 	@PostMapping
-	public String createAccount() {
-		return ("<h1> new user <h1>");
+	public User createAccount(@RequestBody User newUser) {
+		return userService.createUser(newUser);
 	}
 	
 	@GetMapping("/{id}")
-	@PreAuthorize("#id == principal.user.username or hasRole('MANAGER')")
-	public String accessUsersInfo(Principal a) {
-		//System.out.println(((UserPrincipal)((UsernamePasswordAuthenticationToken)a).getPrincipal()).user);
-		return ("<h1> Welcome <h1>");
+	@PreAuthorize("(!hasRole('MANAGER') AND #id == principal.user.id ) OR hasRole('MANAGER')")
+	public Optional<User> accessUsersInfo(@PathVariable long id) {
+		return userService.getUserById(id);
 	}
 
 	@PatchMapping("/{id}")
-	@PreAuthorize("#id == principal.user.username")
-	public String updateUserInfo() {
-		return ("<h1> Welcome <h1>");
+	@PreAuthorize("#id == principal.user.id")
+	public User updateUserInfo(@PathVariable long id, @RequestBody Map<Object, Object> patchedUser) {
+		return userService.patchUser(id, patchedUser);
 	}
 	
 	@DeleteMapping("/{id}")
 	@PreAuthorize("hasRole('MANAGER')")
-	public String deleteAccount() {
-		return ("<h1> Welcome <h1>");
+	public void deleteAccount(@PathVariable long id) {
+		userService.deleteUser(id);
 	}
 }
