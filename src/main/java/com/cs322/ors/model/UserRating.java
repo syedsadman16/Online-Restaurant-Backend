783fcc4486149c1aa89e7b690b1bd40a8716fee0
Syed Sadman <syedsadman16@gmail.com>
2020-10-27 13:13:01 -0400

    UserRating model

--- src/main/java/com/cs322/ors/model/UserRating.java
+++ src/main/java/com/cs322/ors/model/UserRating.java
@@ -1,4 +1,43 @@
 package com.cs322.ors.model;
 
+import javax.persistence.*;
+
 public class UserRating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private int rating; // 1-5 star rating
+    @OneToMany
+    private Order orderId;
+
+    public UserRating(long id, int rating, Order orderId) {
+        this.id = id;
+        this.rating = rating;
+        this.orderId = orderId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Order getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Order orderId) {
+        this.orderId = orderId;
+    }
 }
