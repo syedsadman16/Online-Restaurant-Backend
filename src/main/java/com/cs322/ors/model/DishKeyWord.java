76b5471e19d209928ddd1ffa72cd8f2169e7d12b
Abir Deb <Debabir91@gmail.com>
2020-10-28 01:05:25 -0400

    DishKeyWord model
    finished dishkeyword model, didnt include FK chefid as it felt unnecessay. will finish the other 2 models soon

---
+++ src/main/java/com/cs322/ors/model/DishKeyWord.java
@@ -0,0 +1,63 @@
+package com.cs322.ors.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "KeyWords")
+public class DishKeyWord {
+	
+	@Id
+	@GeneratedValue  //auto by default
+	private long Id;
+	
+	private String keyWord;
+	
+	@ManyToOne //multiple dishes can be marked by same keyword	
+	private Dish dish;
+	
+	// I didn't relate this class to chef users because chefID did not feel needed in the context. 
+	// we just need to relate a dish to a category/keyword
+
+	public DishKeyWord() {
+
+	}
+	
+	public DishKeyWord(long id, String keyWord, Dish dish) {
+		super();
+		Id = id;
+		this.keyWord = keyWord;
+		this.dish = dish;
+	}
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getKeyWord() {
+		return keyWord;
+	}
+
+	public void setKeyWord(String keyWord) {
+		this.keyWord = keyWord;
+	}
+
+	public Dish getDish() {
+		return dish;
+	}
+
+	public void setDish(Dish dish) {
+		this.dish = dish;
+	}
+
+	
+	
+	
+}
